3255196a966770fb9698a4ad241950188b3e9685
Nic Aitch <nic@nicinabox.com>
2015-04-24 16:41:44 -0500

    A basic caching layer

---
+++ pool.js
@@ -0,0 +1,49 @@
+var RSVP = require('RSVP');
+
+var CACHE_MINTUES = 5;
+
+var keys = {};
+var store = {};
+
+var pool = {
+  set: function (key, value) {
+    var date = new Date();
+    date.setMinutes(date.getMinutes() + CACHE_MINTUES);
+
+    keys[key] = {
+      expires: date.getTime()
+    };
+
+    store[key] = value;
+  },
+
+  get: function (key) {
+    var timestamp = new Date().getTime();
+    var item = keys[key];
+
+    return new RSVP.Promise(function (resolve) {
+      if (!item) {
+        resolve();
+        return;
+      }
+
+      if (timestamp >= item.expires) {
+        this.remove(key);
+        resolve();
+      } else {
+        resolve(store[key]);
+      }
+    });
+  },
+
+  remove: function (key) {
+    this.expire(key);
+    delete store[key];
+  },
+
+  expire: function (key) {
+    delete keys[key];
+  },
+};
+
+module.exports = pool;
